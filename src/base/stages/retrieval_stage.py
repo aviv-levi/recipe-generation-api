0dfb02bb75cccc2998fd3b8966bdb6fda2cfe946
AVIV LEVI <60aviv60@gmail.com>
2024-05-18 23:59:42 +0300

    feat: finish retrieval stage

--- src/base/stages/retrieval_stage.py
+++ src/base/stages/retrieval_stage.py
@@ -1,3 +1,4 @@
+from transformers import DPRContextEncoderTokenizer, DPRContextEncoder
 from src.base.base_stage import BaseStage
 
 __all__ = ['RetrievalStage']
@@ -5,6 +6,21 @@ __all__ = ['RetrievalStage']
 
 class RetrievalStage(BaseStage):
 
+    def __init__(self, tokenizer, model, vector_db, recipes):
+        super().__init__()
+        self._k = 5
+        self._tokenizer = tokenizer
+        self._model = model
+        self._vector_db = vector_db
+        self._recipes = recipes
+
+    def _encode_query(self, query):
+        inputs = self._tokenizer([query], return_tensors='pt', padding=True, truncation=True, max_length=512)
+        embeddings = self._model(**inputs).pooler_output
+        return embeddings.detach().numpy()
+
     def run(self, data):
-        print('RetrievalStage')
-        super().run(data)
+        query_embedding = self._encode_query(query=data)
+        vectors_distances, vectors_indices = self._vector_db.search(query_embedding, self._k)
+        relevant_recipes = [self._recipes[i] for i in vectors_indices[0]]
+        super().run(relevant_recipes)
