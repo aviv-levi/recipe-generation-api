0d18ac4a978f8f7ca5435d2c46718b03fac6ee6e
AVIV LEVI <60aviv60@gmail.com>
2024-05-19 00:23:25 +0300

    feat: finish initializer

---
+++ src/initializer.py
@@ -0,0 +1,30 @@
+import os
+import datasets
+from src.base.facade import Facade
+import faiss
+from transformers import DPRContextEncoderTokenizer, DPRContextEncoder, BartTokenizer, BartForConditionalGeneration
+
+class Initializer:
+
+    def initialize_vector_db(self):
+        vector_db_path = os.getenv("VECTOR_DB_PATH", "notebooks/recipes_index.idx")
+        vector_db = faiss.read_index(vector_db_path)
+        return vector_db
+
+    def initialize_recipes(self):
+        dataset_path = os.getenv("DATASET_PATH", "notebooks/dataset")
+        dataset = datasets.load_from_disk(dataset_path)
+        recipes = [entry['steps'] for entry in dataset['train']]
+        return recipes
+
+    def initialize(self) -> Facade:
+        recipes = self.initialize_recipes()
+        vector_db = self.initialize_vector_db()
+        tokenizer = DPRContextEncoderTokenizer.from_pretrained('facebook/dpr-ctx_encoder-multiset-base')
+        model = DPRContextEncoder.from_pretrained('facebook/dpr-ctx_encoder-multiset-base')
+        bart_tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+        bart_model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+
+        return Facade(recipes=recipes, vector_db=vector_db, tokenizer=tokenizer, model=model,
+                      bart_tokenizer=bart_tokenizer, bart_model=bart_model)
+
